889972f72261c5aef76e29a281cd38c37cceabbb
lehuynhanhtai <lehuynhanhtai@gmail.com>
2024-12-31 08:53:53 +0700

    feat: add section hot movie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from "./_components/Banner";
+import HotMovie from "./_components/HotMovie";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <HotMovie />
     </div>
   );
 }
